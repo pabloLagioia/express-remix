cc4b3898ab518a9d640474c733e413fd5fb4ed73
Pablo Lagioia <pablo.lagioia@gmail.com>
2018-02-14 15:59:13 -0300

    added readme

---
+++ index.js
@@ -0,0 +1,133 @@
+class MiddlewareDependencyError extends Error {
+
+  constructor(middelwareName, dependency, url, method) {
+    super(`Middleware '${middelwareName}' dependency '${dependency}' not met on '${method} ${url}'`);
+    this.middelwareName = middelwareName;
+    this.dependency = dependency;
+    this.url = url;
+    this.status = 400;
+  }
+
+}
+
+class ValidationMiddlewareError extends Error {
+
+  constructor(middelwareName, url, method, message = "") {
+    super(`Validation error: '${middelwareName}' on '${method} ${url}'. ${message}`);
+    this.middelwareName = middelwareName;
+    this.url = url;
+    this.status = 400;
+  }
+
+}
+
+const getFieldsFromRequest = req => ({"url": req.originalUrl, "method": req.method, "path": req.path});
+const getData = req => Object.assign({}, req.params, req.query, req.body, req.middlewares, req.middlewares.nameless, getFieldsFromRequest(req));
+const ensureMiddlewares = req => {
+  if (!req.middlewares) {
+    req.middlewares = {};
+  }
+};
+const areDependenciesNotMet = (data, dependencies) => dependencies.some(dependency => {
+
+  let dep = data[dependency];
+
+  return dep === undefined || dep === null
+
+});
+
+const namedMiddleware = (name, fn, dependencies = []) => async (req, res, next) => {
+  
+  ensureMiddlewares(req);
+
+  try {
+
+    const data = getData(req);
+
+    if (areDependenciesNotMet(data, dependencies)) {
+      throw new MiddlewareDependencyError(name, dependency, req.originalUrl, req.method);
+    }
+
+    const middlewareResponse = await fn(data);
+
+    req.middlewares[name] = middlewareResponse;
+
+    next();
+
+  } catch (e) {
+    next(e);
+  }
+
+};
+
+const middleware = (name, fn, dependencies, exports) => {
+
+  if (typeof name === "string") {
+    return namedMiddleware(name, fn, dependencies);
+  }
+
+  return namedMiddleware("nameless", name, fn);
+
+}
+
+const respond = (fn, dependencies = []) => async (req, res, next) => {
+
+  ensureMiddlewares(req);
+
+  try {
+
+    const data = getData(req);
+
+    if (areDependenciesNotMet(data, dependencies)) {
+      throw new MiddlewareDependencyError("respond", dependency, req.originalUrl, req.method);
+    }
+
+    const response = await fn(data);
+
+    res.status(response.status || 200);
+
+    response.headers && Object.keys(response.headers).forEach(header => res.setHeader(header, response.headers[header]));
+
+    res.end(typeof response.body === "object" ? JSON.stringify(response.body) : response.body);
+
+  } catch(e) {
+    next(e);
+  }
+
+};
+
+const validationMiddleware = (name, fn, message) => (req, res, next) => {
+  
+  ensureMiddlewares(req)
+
+  try {
+
+    const data = getData(req);
+
+    if (!fn(data)) {
+      throw new ValidationMiddlewareError(name, req.originalUrl, req.method, message);
+    }
+
+    next();
+
+  } catch (e) {
+    next(e);
+  }
+
+};
+
+const spreadMiddleware = (name, fields) => (req, res, next) => {
+
+  const targetMiddleware = req.middlewares[name];
+
+  fields.forEach(fieldName => {
+    req.body[fieldName] = targetMiddleware[fieldName];
+  });
+
+  next();
+
+};
+
+module.exports = {
+  middleware, respond, spreadMiddleware, validationMiddleware, MiddlewareDependencyError, ValidationMiddlewareError
+}
---
+++ readme.md
@@ -0,0 +1,79 @@
+#What is this?
+It's a library to make it easier to write chainable middlewares for express
+
+#Why?
+- Assigning things to req doesn't look good
+- I'd like to forget about req, res, next and just write my code
+- I'd like to reuse the middlewares somewhere else
+- I want to easily validate the data
+- I just want the data to be there and be able to take it from other middlewares
+
+#Example
+
+```
+const { middleware, respond, validationMiddleware, spreadMiddleware } = require("express-remix");
+
+const saveTextMessage = middleware("saveTextMessage", ({from, to, text, recipientType, senderType, inspectionOrderId, labels, userId, storeId, customerId, read}) => Messages.save({
+  recipientType, 
+  senderType,
+  from,
+  to,
+  text,
+  inspectionOrderId,
+  labels,
+  userId,
+  storeId,
+  customerId,
+  read
+}), ["from", "to", "text"]);
+
+...
+
+router.post("/customer/:customerId/text-message", 
+  middleware("customer", ({customerId}) => Customers.get(customerId)),
+  validationMiddleware("validate customer exists", ({customer}) => customer),
+  validationMiddleware("validate customer has phone number", ({customer}) => customer.normalizedMobilePhone),
+  middleware("store", ({customer}) => Stores.get(customer.storeId)),
+  middleware(({customer, store, text}) => {
+    return {
+      "recipientType": "customer", 
+      "senderType": "store",
+      "from": store.interfacePhoneNumber,
+      "to": customer.normalizedMobilePhone,
+      "customerId": customer._id,
+      "storeId": store._id,
+      "read": true
+    }
+  }),
+  saveTextMessage,
+  ok
+);
+```
+
+Middlewares work as pipelines but the result of the previous middleware execution is stored in the middleware itself.
+The library makes it easy to retrieve this results by merging them into a big object so you don't have to worry where you're getting the data from.
+A middleware can depend on another middleware:
+
+```
+const miMiddleware = middleware("customer", ({customerId}) => Customers.get(customerId));
+const middlewareThatDependsOnCustomerMiddleware = middleware(({customer}) => console.log(`Here's the customer ${customer}`), ["customer"]);
+```
+## Named middleware 
+`middleware(name, fn, dependencyArray);`
+
+## Unnamed middleware
+`middleware(fn, dependencyArray);`
+
+##Response middleware:
+`respond(fn, dependencyArray);`
+
+Example:
+
+```
+const sendCustomerJson = respond({customer} => {
+  return {
+    "headers": { "Content-Type": "application/json" },
+    "body": JSON.stringify(customer)
+  }
+});
+```
